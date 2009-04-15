0dd3a0d3cf6349e9f2d87405ea262a8d551e9f0d
Deepak <vrdeepak87@yahoo.com>
2009-04-15 12:54:53 +0530

    Resolved bug in adding an empty comment

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,12 +1,13 @@
 class BlogController < ApplicationController
 
 def list
-#@blogs=Blog.find(:all)
 @blogs=Blog.paginate(:page => params[:page], :per_page => 5, :order => 'created_at DESC')
+#Storing all the blogs in the instance variable.Paginate does the job of "find".
 end
 
 def show
 @blog=Blog.find(params[:id])
+#Searching a particular blod using "id" and storing it in @blog
 end
 
 def new
@@ -16,8 +17,10 @@ end
 def create
   @blog=Blog.new(params[:blog])
   if @blog.save
+    #action to be performed on a successive save 
     redirect_to :action => 'list'
   else
+    #action to be performed on a failure
     render :action => 'new'
   end
 end
