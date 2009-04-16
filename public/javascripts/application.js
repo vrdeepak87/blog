4c107f049a5e6161ad346ecd0a23be1dddf13eb3
Deepak <vrdeepak87@yahoo.com>
2009-04-16 18:28:53 +0530

    Getting Started with Ajax

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+
+$(document).ready(function() {
+	$('#addcomment').submit(function() {
+	$.post($(this).attr('action'), $(this).serialize(), null, 'script');
+	return false;
+	});
+
+});
+
+
+jQuery.ajaxSetup({ 
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")} 
+})
